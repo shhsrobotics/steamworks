32d20a2a1297be5d50d9c4643742d48ec3e23297
shhsrobotics <shhsrobotics@gmail.com>
2017-01-14 12:42:35 -0500

    added filter to find reflective tape

--- java_2017/src/org/usfirst/frc/team486/robot/camera/Prep.java
+++ java_2017/src/org/usfirst/frc/team486/robot/camera/Prep.java
@@ -7,8 +7,8 @@ import org.opencv.core.Scalar;
 public class Prep {
 	
 	//opencv uses ranges (H: 0-180, S: 0-255, V:0-255)
-	Scalar lower_hsv = new Scalar(50,100,100);
-	Scalar upper_hsv = new Scalar(100,255,255);
+	Scalar lower_hsv = new Scalar(50,20,240);
+	Scalar upper_hsv = new Scalar(90,255,255);
 	
 	public void filter_hsv(Mat hsv){
 		Core.inRange(hsv, this.lower_hsv, this.upper_hsv, hsv);
