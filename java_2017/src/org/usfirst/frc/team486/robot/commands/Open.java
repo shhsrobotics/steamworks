e669ddc2eaa99ab49f555b2dc76e6ceba3013bf9
shhsrobotics <shhsrobotics@gmail.com>
2017-02-15 11:40:38 -0500

    regulator now functioning

--- java_2017/src/org/usfirst/frc/team486/robot/commands/Open.java
+++ java_2017/src/org/usfirst/frc/team486/robot/commands/Open.java
@@ -32,12 +32,12 @@ public class Open extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.regulator.open();
+    	Robot.regulator.close();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.regulator.open();
+    	Robot.regulator.close();
     }
 }
